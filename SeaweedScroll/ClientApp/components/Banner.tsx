0a642cc5776ff745ef0387f90c14e54211c74657
Sam Rockwell <wfrockwell@gmail.com>
2018-10-10 18:14:36 -0400

    Implement basic page grid layout. Implement basic but stationary header

---
+++ SeaweedScroll/ClientApp/components/Banner.tsx
@@ -0,0 +1,21 @@
+﻿import * as React from 'react'; 
+
+export class Banner extends React.Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div id="banner">
+                <div id="mascotPicture" className="fullRowImg" />
+                <div id="websiteTitle">Seaweed Scroll</div>
+                <div id="vision" className="bannerItem">Vision</div>
+                <div id="journey" className="bannerItem">Journey</div>
+                <div id="partner" className="bannerItem">Partner</div>
+                <div id="art" className="bannerItem">Art</div>
+                <div id="contact" className="bannerItem">Contact</div>
+            </div>
+        );
+    }
+}
--- SeaweedScroll/ClientApp/components/Home.tsx
+++ SeaweedScroll/ClientApp/components/Home.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { Banner } from './Banner';
 
 export default class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
-        return <div></div>;
+        return (
+            <div>
+                <Banner />
+                <div id="HomeContent">
+                    <div id="SeaweedScroll" className="fullRowImg inspirationRow">Our Story</div>
+                    <div id="Vision" className="fullRowImg inspirationRow">Vision</div>
+                    <div id="Journey" className="fullRowImg inspirationRow">Journey</div>
+                    <div id="Partner" className="fullRowImg inspirationRow">Partner</div>
+                    <div id="Art" className="fullRowImg inspirationRow">Art</div>
+                    <div id="Contact" className="fullRowImg inspirationRow">Contact</div>
+                </div>
+            </div>
+        );
     }
 }
